16e07f1d3713a83826051244607a2a5f82051e18
Chris <christian.maechler@brix.ch>
2023-12-21 18:05:47 +0100

    various improvements and documentation

--- src/main/java/ch/brix/camunda/connector/util/templateGenerator/DefaultTexts.java
+++
@@ -1,5 +0,0 @@
-package ch.brix.camunda.connector.util.templateGenerator;
-
-public class DefaultTexts {
-    public static final String RESULT_VARIABLEs = "Name of the variable to store the response in.";
-}
